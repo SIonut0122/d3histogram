103e161e92751b0ccaceb00777957eba7a7ddb06
SIonut0122 <sionut0122@yahoo.com>
2022-06-27 10:18:51 +0300

    'test'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [number, setNumber] = useState(0);
+
+  useEffect(() => {
+    document.querySelector('.slbz').innerHTML = number;
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <section>
+          <div className='content'>
+              <span className='slbz'>0</span>
+          </div>
+       </section>
+ 
     </div>
   );
 }
